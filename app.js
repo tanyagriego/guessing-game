efa4d13a942842ebf736c0959703543703d75d51
tanyagriego <tanya.griego@gmail.com>
2017-08-06 15:02:10 -0700

    updated else/if prompts

--- app.js
+++ app.js
@@ -6,13 +6,15 @@
 
   var favoriteThing = prompt("First things first. Is Grandmother's very favorite thing her green dress?", "yes/no");
   var favoriteThingLowerCase = favoriteThing.toLowerCase()
-  if (favoriteThingLowerCase == "yes" || favoriteThingLowerCase == "y"){
+  if (favoriteThingLowerCase == "yes" || favoriteThingLowerCase == "y")
+  {
     alert("Gold star for you " +userName + "! That's correct!");
     console.log("user responded yes to question one")
   }
   else if (favoriteThingLowerCase == "no" || favoriteThingLowerCase == "n") {alert ("Negative, ghost rider. Green dress is number one. No gold star for " +userName + ". Bummer!");
-  console.log("user anwsners no to question one")}
-  else {alert ("That's not a real answer. I asked for yes or no!");
+  console.log("user anwsners no to question one")
+  }
+  else {alert ("Yes or no answers only, please.");
   }
 
   var crush = prompt("Is Grandmother's #1 celebrity crush Jason Hardy?", "yes/no");
@@ -22,18 +24,22 @@
     alert("Gold Start for you " +userName + "! Jason is a celebrity in Grandmother's eyes. *Aww*");
     console.log("user responded yes to question two.")
   }
-
-  else {alert ("You're totally wrong right now.");
+  else if (crushLowerCase == "no" || crushLowerCase == "n") {alert ("You're totally wrong right now.");
   console.log("user responded no to questions two.")
-}
-var mona = prompt("Has Grandmother touched the Mona Lisa?", "yes/no");
-var monaLowerCase = mona.toLowerCase()
-if (monaLowerCase == "yes" || monaLowerCase == "y")
+  }
+  else {alert ("Yes or no answers only, please.");
+  }
+
+  var mona = prompt("Has Grandmother touched the Mona Lisa?", "yes/no");
+  var monaLowerCase = mona.toLowerCase()
+  if (monaLowerCase == "yes" || monaLowerCase == "y")
 {
   alert("Trick question! She didn't touch the Mona Lisa, but boy, she sure got close! No gold star this time.");
   console.log("user responded no to question three.")
 }
 
-else {alert ("You would think that. Classic " +userName + ". Smarty Pants.");
+else if (monaLowerCase == "no" || monaLowerCase == "n") {alert ("You would think that. Classic " +userName + ". Smarty Pants.");
 console.log("user responded no to question three.")
+}
+else {alert ("Yes or no answers only, please.")
 }
