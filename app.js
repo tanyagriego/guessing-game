cedd61000e83db8fd4730d5aba36171ced089278
tanyagriego <tanya.griego@gmail.com>
2017-08-09 19:58:21 -0700

    answers and responses on webpage

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
     var correct = 0
     var incorrect = 0
+    var message=""
+    var userName=""
 
+    function triviaGame() {
+      Q1(); Q2(); Q3(); Q4(); finalScore();
+    }
 
-    var userName = prompt("Oh, hi. Wanna learn about Grandmother? Tell me your name.", "");
+//name//
+  function Q1 (){
+    userName = prompt("Oh, hi. Wanna learn about Grandmother? Tell me your name.", "");
     alert("Well hey there " +userName + "! Welcome to Grandmother Trivia!");
     console.log("userName is " +userName)
-  //favoriteThing//
+  }
+
+//favoriteThing//
+  function Q2 (){
+    var message = ""
     var favoriteThing = prompt("First things first. Is Grandmother's very favorite thing her green dress?", "yes/no");
     var favoriteThingLowerCase = favoriteThing.toLowerCase()
     if (favoriteThingLowerCase == "yes" || favoriteThingLowerCase == "y")
     {
-      alert("Gold star for you " +userName + "! That's correct!");
+      message = "Gold star for you"  +userName + "! That's correct!";
       correct = correct + 1;
       console.log("user responded yes to question one")
     }
     else if (favoriteThingLowerCase == "no" || favoriteThingLowerCase == "n")
     {
-      alert ("Negative, ghost rider. Green dress is number one. No gold star for " +userName + ". Bummer!");
+      message = "Negative, ghost rider. Green dress is number one. No gold star for " +userName + ". Bummer!";
       incorrect = incorrect +1;
-    console.log("user anwsners no to question one")
+      console.log("user anwsners no to question one")
     }
     else
     {
-      alert ("Yes or no answers only, please.");
+      message="Yes or no answers only, please.";
     }
-  //crush//
+    // document.getElementById('special-section').innerHTML = message;
+  }
+
+//crush//
+  function Q3 (){
     var crush = prompt("Is Grandmother's #1 celebrity crush Jason Hardy?", "yes/no");
     var crushLowerCase = crush.toLowerCase()
     if (crushLowerCase == "yes" || crushLowerCase == "y")
@@ -41,7 +56,10 @@
     }
     else {alert ("Yes or no answers only, please.");
     }
+  }
+
 //mona//
+  function Q4 (){
     var mona = prompt("Has Grandmother touched the Mona Lisa?", "yes/no");
     var monaLowerCase = mona.toLowerCase()
     if (monaLowerCase == "yes" || monaLowerCase == "y")
@@ -58,7 +76,10 @@
     }
     else {alert ("Yes or no answers only, please.")
     }
-  //age//
+  }
+
+//age//
+  function Q5 (){
     var age = prompt("How old is Grandmother?");
     if (age == "87")
     {
@@ -68,15 +89,19 @@
     }
     else if (age >= 88)
     {
-      alert ("Don't be rude. She's not dead. Your guess is too high!")
-    incorrect = incorrect + 1;
-    }
+      alert ("Don't be rude. She's not dead. Your guess is too high! Try again.")
+      incorrect = incorrect + 1;
+      console.log ("user responded incorrectly to question four")
+  }
     else if (age <=86) {alert ("Grandmother thanks you for your kindness. Your guess is too low.")
     incorrect = incorrect + 1;
   }
     console.log("user responded incorrectly to question four")
+  }
+//while loop to repeat a question (as in a wrong answer to Grandmother's age)
 
-
-
+//final score//
+  function finalScore () {
     alert("You earned " + correct + " of 4 gold stars.")
     alert("You missed " + incorrect + " of 4 gold stars.")
+  }
